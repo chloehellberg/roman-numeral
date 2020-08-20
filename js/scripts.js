e12e87e834864c5565789f6d3cea1184e0d8323d
Michael Watts <wattsjmichael@gmail.com>
2020-08-20 12:03:51 -0700

    adds initial testing
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
     $("#formOne").submit(function(event){
         event.preventDefault();
+        const value = parseInt($("input#number").val());
+
+        if(value < 1 || value > 3999) {
+            alert ("Enter a number between 1 and 3999");
+        }
     })    
 });
+
+// function numberToRoman(value) {
+//     let romanNumerals = {I:1, IV:4, V:5, IX:9, X:10}
+//     let value = "";
+
+//     if(value < 1 || value > 3999) {
+//         alert ("Enter a number between 1 and 3999");
+//     }
+// }
+
+// console.log(numberToRoman(1));
+
+// // 1. No letters, only intergers
+// // 2. No intergers higher than 3999
+// // 3. No negative numbers
+// // 4. No more than 3 consective roman numerals
+// // 5.
+
+// let romanNumber = ["I","V","X","L","C","D","M"]
+// let value;
+
+// function numberToRoman(romanNumber, value) {
+//     value = 1;
+    
+// }
+// value@value.length-1 <= 3 return I's
+
